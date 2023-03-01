4d83b9de0b28827919d0c88d252711fec095f350
DIANELLY VENTURA <DMVCode@outlook.com>
2023-02-28 22:05:32 -0500

    Add ProductsIndex

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,10 @@
+import { ProductsIndex } from "./ProductsIndex";
+
 export function Content() {
   return (
     <div>
       <h1>Welcome to My Mini Capstone</h1>
+      <ProductsIndex />
     </div>
   );
 }
